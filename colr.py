ce32955734a9c02ce054fa01123ff05c0327d8d4
Rishi Prasad Narayan Singh <122547010+Rishiprogramer@users.noreply.github.com>
2023-04-18 21:22:53 +0530

    Add files via upload

---
+++ colr.py
@@ -0,0 +1,12 @@
+class clor:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    ATTACKTYPE = '\033[36m'
+    ATTACKCHOSEN = '\033[33m'
+    ATTACKGIVETAKE = '\033[35m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
